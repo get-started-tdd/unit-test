03048b454493d70b40283995f5288a7dd345a373
xiaoliang <32414154@qq.com>
2015-05-17 19:06:42 +0800

    添加代码使测试能够编译,并运行测试查看失败的原因

---
+++ src/main/java/com/lx/MemberCounter.java
@@ -0,0 +1,16 @@
+package com.lx;
+
+import java.util.List;
+
+/**
+ * Created by L.x on 15-5-17.
+ */
+public class MemberCounter {
+    public MemberCounter(int m) {
+
+    }
+
+    public List<Integer> countOff(int numberOfMembers) {
+        return null;
+    }
+}
--- src/test/java/test/com/lx/MemberCounterTest.java
+++ src/test/java/test/com/lx/MemberCounterTest.java
@@ -1,10 +1,12 @@
 package test.com.lx;
 
+import com.lx.MemberCounter;
 import org.junit.Test;
 
 import java.util.Collections;
 
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
 
 /**
  * Created by L.x on 15-5-17.
@@ -14,6 +16,6 @@ public class MemberCounterTest {
     public void noMembers() throws Exception {
         MemberCounter counter = new MemberCounter(3);
 
-        assertThat(counter.countOff(0), equalTo(Collections.emptyList()));
+        assertThat(counter.countOff(0), equalTo(Collections.<Integer>emptyList()));
     }
 }
