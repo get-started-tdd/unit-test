98af0515b3eff1e43f43e3247bd6d172d6bd5da6
xiaoliang <32414154@qq.com>
2015-05-17 20:01:00 +0800

    修改预期值

--- src/test/java/test/com/lx/MemberCounterExampleTest.java
+++ src/test/java/test/com/lx/MemberCounterExampleTest.java
@@ -17,7 +17,14 @@ public class MemberCounterExampleTest {
 
     @Test
     public void countOff() throws Exception {
-        List<Integer> expected = Arrays.asList(3, 6, 9, 12, 15, 18, 1, 7, 13, 19, 5, 16, 8, 2, 4, 14, 17, 10, 11, 20);
+        //例子来自于http://jerval.iteye.com/blog/1068563，居然博主给出的结果是错误的:(
+        List<Integer> expected = Arrays.asList(
+                3, 6, 9, 12, 15, 18,
+                1, 5, 10, 14, 19,
+                4, 11, 17,
+                7, 16, 8,
+                2, 13, 20
+        );
         assertThat(counter.countOff(20), equalTo(expected));
     }
 
