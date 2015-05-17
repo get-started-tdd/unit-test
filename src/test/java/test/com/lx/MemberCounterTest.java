c15571c5e836a43e98cb46b9411252a5d0863457
xiaoliang <32414154@qq.com>
2015-05-17 19:00:40 +0800

    #测试 添加一个测试确认当前的测试环境是否配置正确

---
+++ src/test/java/test/com/lx/MemberCounterTest.java
@@ -0,0 +1,16 @@
+package test.com.lx;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Created by L.x on 15-5-17.
+ */
+public class MemberCounterTest {
+    @Test
+    public void scaffoldingIsReady() throws Exception {
+        assertThat(true, equalTo(true));
+    }
+}
